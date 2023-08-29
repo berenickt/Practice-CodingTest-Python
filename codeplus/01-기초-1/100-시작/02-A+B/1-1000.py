88037e51e758434bd9059f9b61aeb3de50a0570a
Beren <berenickt99@gmail.com>
2023-08-30 00:07:50 +0900

    FIX:1-1000

--- codeplus/01-기초-1/100-시작/02-A+B/1-1000.py
+++ codeplus/01-기초-1/100-시작/02-A+B/1-1000.py
@@ -1,3 +1,11 @@
 # 💡 A+B - 1 @https://www.acmicpc.net/problem/1000
+
+# 이썬 코드로, 두 개의 정수를 입력받아 각각 변수 a와 b에 저장하는 코드
+# 📌 input()는 사용자로부터 입력을 받습니다.
+# 📌 split()는 입력받은 문자열을 공백을 기준으로 분리하여 리스트로 만듭니다.
+# 📌 map(int, ...) 함수는 리스트의 각 요소를 int 함수를 사용하여 정수로 변환
+# e.g. "3 5"라고 입력하면, a에는 3이, b에는 5가 저장됨 
+# print(input().split())
+
 a, b = map(int, input().split())
 print(a + b)
