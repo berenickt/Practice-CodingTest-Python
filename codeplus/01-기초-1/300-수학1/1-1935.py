102c1d07a1dad16cbcceac9ea2f1e08688fce32a
Beren <berenickt99@gmail.com>
2023-08-30 23:39:52 +0900

    ADD:300-수학1

---
+++ codeplus/01-기초-1/300-수학1/1-1935.py
@@ -0,0 +1,6 @@
+# 💡 나머지 @https://www.acmicpc.net/problem/1935
+a,b,c = map(int,input().split())
+print((a % c + b % c) % c)
+print((a % c + b % c) % c)
+print((a % c * b % c) % c)
+print((a % c * b % c) % c)
---
+++ codeplus/01-기초-1/300-수학1/7-10872.py
@@ -0,0 +1,7 @@
+# 💡 팩토리얼 @https://www.acmicpc.net/problem/10872
+def factorial(n):
+  if n == 0: return 1
+  else: return n * factorial(n-1)
+
+n = int(input())
+print(factorial(n))
