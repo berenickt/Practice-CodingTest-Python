0a50e0719402382ccaf46c97b4571f7b8123800e
Beren <berenickt99@gmail.com>
2023-08-31 23:39:52 +0900

    ADD:540-브루트포스-비트마스크

---
+++ codeplus/02-기초-2/540-브루트포스-비트마스크/2-1182.py
@@ -0,0 +1,10 @@
+# 💡 부분집합의 합 @https://www.acmicpc.net/problem/1182
+n,m = map(int,input().split())
+a = list(map(int,input().split()))
+ans = 0
+
+for i in range(1, (1<<n)):
+  s = sum(a[k] for k in range(n) if (i & (1<<k)) > 0)
+  if m == s: ans += 1
+
+print(ans)
