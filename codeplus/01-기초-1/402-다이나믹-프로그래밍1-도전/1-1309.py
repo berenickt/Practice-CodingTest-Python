1a8bb0d0667b4bd787ee8e02bf4eff87c892609e
Beren <berenickt99@gmail.com>
2023-08-31 21:04:25 +0900

    ADD:402-다이나믹-프로그래밍1-도전

---
+++ codeplus/01-기초-1/402-다이나믹-프로그래밍1-도전/1-1309.py
@@ -0,0 +1,16 @@
+# 💡 동물원 @https://www.acmicpc.net/problem/1309
+d = [0] * 100001
+s = [0] * 100001
+n = int(input())
+d[0] = 1
+s[0] = 1
+d[1] = 2
+s[1] = d[0] + d[1]
+
+for i in range(2, n+1):
+  d[i] = d[i-1] + 2*s[i-2]
+  s[i] = s[i-1] + d[i]
+  d[i] %= 9901
+  s[i] %= 9901
+
+print(s[n])
