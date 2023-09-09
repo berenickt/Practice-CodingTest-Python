510438cd968ea77b68b6f25f6431cbe92ab77d0d
Beren <berenickt99@gmail.com>
2023-09-09 21:30:07 +0900

    FIX:02-A+B

--- codeplus/01-기초-1/100-시작/02-A+B/4-10951.py
+++ codeplus/01-기초-1/100-시작/02-A+B/4-10951.py
@@ -1,7 +1,9 @@
 # 💡 A+B - 4 📚 https://www.acmicpc.net/problem/10951
 while True:
     try:
+        # 사용자로부터 두 정수를 입력받고, 공백을 기준으로 분리하여 정수 변수 a와 b에 할당
         a, b = map(int, input().split())
         print(a + b)
     except:
+        # 예외가 발생하면(예: 입력이 정수가 아닌 경우), 루프를 종료
         break
--- codeplus/01-기초-1/100-시작/02-A+B/5-10952.py
+++ codeplus/01-기초-1/100-시작/02-A+B/5-10952.py
@@ -1,7 +1,12 @@
 # 💡 A+B - 5 📚 https://www.acmicpc.net/problem/10952
 while True:
+  # 사용자로부터 두 정수를 입력받고, 입력값을 공백을 기준으로 분리하여 정수 변수 a와 b에 할당
     a, b = map(int, input().split())
+
+    # 만약 a와 b가 모두 0이라면, 루프를 종료
     if (a == 0 and b == 0):
         break
+
+    # a와 b의 합을 계산하고 출력
     else:
         print(a + b)
