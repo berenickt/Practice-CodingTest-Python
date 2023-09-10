7d1be3c98cc1c6fa8da3891386bda759828101ab
Beren <berenickt99@gmail.com>
2023-09-10 22:43:38 +0900

    ADD:301-수학1-연습

--- codeplus/01-기초-1/301-수학1-연습/4-1212.py
+++ codeplus/01-기초-1/301-수학1-연습/4-1212.py
@@ -1,13 +1,25 @@
 # 💡 8진수 2진수 📚 https://www.acmicpc.net/problem/1212
+# 8진수를 이진수로 변환하기 위한 8진수-이진수 매핑 리스트를 생성
 eight = ["000", "001", "010", "011", "100", "101", "110", "111"]
+
+# 사용자로부터 문자열을 입력받음
 s = input()
+
 start = True
+
+# 이진수 변환 결과를 저장할 변수를 초기화
 ans = ""
 
+# 입력된 문자열이 "0"인 경우, 결과를 "0"으로 설정
 if s == "0":
     ans = "0"
+
+# 입력된 문자열을 순회하면서 처리
 for ch in s:
+    # 문자를 숫자로 변환
     n = ord(ch) - ord("0")
+
+    # 시작 플래그인 start가 True이고, n이 4보다 작은 경우 처리
     if start and n < 4:
         if n == 0:
             continue
@@ -17,8 +29,14 @@ for ch in s:
             ans += "10"
         elif n == 3:
             ans += "11"
+        # start 플래그를 False로 변경
         start = False
     else:
+        # 8진수-이진수 매핑 리스트에서 이진수를 가져와 결과에 추가
         ans += eight[n]
+
+        # start 플래그를 False로 변경
         start = False
+
+# 최종 변환된 이진수를 출력
 print(ans)
--- codeplus/01-기초-1/301-수학1-연습/6-17103.py
+++ codeplus/01-기초-1/301-수학1-연습/6-17103.py
@@ -1,23 +1,43 @@
 # 💡 골드바흐 파티션 📚 https://www.acmicpc.net/problem/17103
+
+# 소수를 체크하는 리스트를 초기화합니다. (인덱스는 0부터 1000000까지)
 check = [False] * 1000001
+
+# 소수를 저장할 리스트를 초기화
 primes = []
 
+# 에라토스테네스의 체 알고리즘을 사용하여 소수를 찾습니다
 for i in range(2, 1000001):
     if check[i] == False:
+        # 소수를 발견하면 primes 리스트에 추가
         primes.append(i)
         j = i + i
         while j <= 1000000:
+            # 소수의 배수들을 체크
             check[j] = True
             j += i
 
-t = int(input())
-for _ in range(t):
+# 테스트 케이스의 개수를 입력받음
+TEST_CASE = int(input())
+
+# 각 테스트 케이스를 처리
+for _ in range(TEST_CASE):
+    # 정수 n을 입력받음
     n = int(input())
+
+    # 소수 쌍의 개수를 세기 위한 변수 ans를 초기화
     ans = 0
+
+    # primes 리스트의 소수들을 반복하면서 소수 쌍을 찾습니다
     for p in primes:
         if n - p >= 2 and p <= n - p:
+            # n - p가 2 이상이고, p가 n - p보다 작을 경우에만 검사
             if check[n - p] == False:
+                # n - p가 소수인 경우, ans를 증가
                 ans += 1
+        # 조건을 만족하지 않으면 반복을 종료
         else:
             break
+
+    # 소수 쌍의 개수를 출력
     print(ans)
