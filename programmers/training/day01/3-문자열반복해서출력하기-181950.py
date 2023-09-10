4f6aec15c650b6be58b6f5946997afe177ceb96a
Beren <berenickt99@gmail.com>
2023-09-10 09:12:06 +0900

    ADD:기초-트레이닝-Day01

---
+++ programmers/training/day01/3-문자열반복해서출력하기-181950.py
@@ -0,0 +1,5 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181950
+a, b = input().strip().split(" ")
+
+for i in range(int(b)):
+    print(a, end="")
---
+++ programmers/training/day01/4-1-대소문자바꿔서출력하기-181949.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181949
+str = input()
+
+for i in str:
+    if i.islower():
+        print(i.upper(), end="")
+    else:
+        print(i.lower(), end="")
