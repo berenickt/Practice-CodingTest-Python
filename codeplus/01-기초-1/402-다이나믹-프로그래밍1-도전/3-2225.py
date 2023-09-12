08f40915ade60c8aa8225363da4ef2274a200f59
Beren <berenickt99@gmail.com>
2023-09-12 21:54:12 +0900

    ADD:402-다이나믹-프로그래밍1-도전

--- codeplus/01-기초-1/402-다이나믹-프로그래밍1-도전/3-2225.py
+++ codeplus/01-기초-1/402-다이나믹-프로그래밍1-도전/3-2225.py
@@ -1,12 +1,23 @@
 # 💡 합분해 📚 https://www.acmicpc.net/problem/2225
+# 모듈러 연산을 위한 상수 설정
 mod = 1000000000
+
+# 입력을 받아서 n과 m에 할당
 n, m = map(int, input().split())
+
+# 배열 d 초기화
 d = [0] * (n + 1)
+
+# 초기값 설정
 d[0] = 1
 
+# m번 반복하는 반복문
 for i in range(1, m + 1):
+    # 1부터 n까지 반복하는 반복문
     for j in range(1, n + 1):
+        # 현재 위치의 값을 이전 값과 더하고 모듈러 연산을 수행
         d[j] += d[j - 1]
         d[j] %= mod
 
+# 결과 출력
 print(d[n])
