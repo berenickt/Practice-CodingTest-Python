8611974ea5181d2a7efcb07d3a79717f9b8a3a11
Beren <berenickt99@gmail.com>
2023-09-15 00:19:13 +0900

    ADD:540-브루트포스-비트마스크

--- codeplus/02-기초-2/540-브루트포스-비트마스크/1-11723.py
+++ codeplus/02-기초-2/540-브루트포스-비트마스크/1-11723.py
@@ -1,27 +1,46 @@
 # 💡 집합 📚 https://www.acmicpc.net/problem/11723
 import sys
 
-n = 20
-m = int(sys.stdin.readline())
-s = 0
+n = 20  # 비트 벡터의 크기 (비트 벡터의 길이)
+m = int(sys.stdin.readline())  # 입력: 명령어의 개수
+s = 0  # 초기 비트 벡터
 
 for _ in range(m):
+    # 입력: 명령어와 숫자 (op는 명령어, num은 명령어와 함께 주어진 숫자)
     op, *num = sys.stdin.readline().split()
+
+    # 숫자가 주어진 경우 0부터 시작하는 인덱스로 변환
     if len(num) > 0:
         x = int(num[0]) - 1
+
+    # 비트 벡터에서 해당 비트를 1로 설정 (OR 연산)
     if op == "add":
         s = s | (1 << x)
+
+    # 비트 벡터에서 해당 비트를 0으로 설정 (AND 연산 후 NOT 연산)
     elif op == "remove":
         s = s & ~(1 << x)
+
     elif op == "check":
+        # 비트 벡터에서 해당 비트의 상태 확인 (AND 연산)
         res = s & (1 << x)
+
+        # 해당 비트가 1로 설정되어 있으면 1 출력
         if res > 0:
             sys.stdout.write("1\n")
+
+        # 해당 비트가 0으로 설정되어 있으면 0 출력
         else:
             sys.stdout.write("0\n")
+
+    # 비트 벡터에서 해당 비트를 반전 (XOR 연산)
     elif op == "toggle":
         s = s ^ (1 << x)
+
+    # 모든 비트를 1로 설정 (2^n - 1로 계산)
     elif op == "all":
         s = (1 << n) - 1
+
+    # 비트 벡터를 모두 0으로 초기화
     else:
         s = 0
--- codeplus/02-기초-2/540-브루트포스-비트마스크/4-14889---2.py
+++ codeplus/02-기초-2/540-브루트포스-비트마스크/4-14889---2.py
@@ -1,34 +1,56 @@
 # 💡 스타트와 링크 📚 https://www.acmicpc.net/problem/14889
+# 입력: 팀원의 수
 n = int(input())
+
+# 입력: 능력치 행렬
 s = [list(map(int, input().split())) for _ in range(n)]
+
+# 결과를 저장할 변수, 초기값은 -1로 설정
 ans = -1
 
+# 모든 가능한 팀 조합을 검사하기 위한 반복문
 for i in range((1 << n)):
+    # 현재 팀의 멤버 수를 저장하는 변수
     cnt = 0
     for j in range(n):
+        # 비트마스크를 사용하여 현재 팀의 멤버 수 계산
         if (i & (1 << j)) > 0:
             cnt += 1
+
+    # 팀의 크기가 반씩 나뉘지 않으면 스킵
     if cnt != n // 2:
         continue
-    first = []
-    second = []
+
+    first = []  # 첫 번째 팀의 멤버를 저장하는 리스트
+    second = []  # 두 번째 팀의 멤버를 저장하는 리스트
+
     for j in range(n):
+        # 비트마스크를 사용하여 팀 멤버를 구분
         if (i & (1 << j)) > 0:
             first += [j]
         else:
             second += [j]
+
+    # 팀의 크기가 반씩 나뉘지 않으면 스킵
     if len(first) != n // 2:
         continue
-    t1 = 0
-    t2 = 0
+
+    t1 = 0  # 첫 번째 팀의 능력치 합을 저장하는 변수
+    t2 = 0  # 두 번째 팀의 능력치 합을 저장하는 변수
+
     for l1 in range(n // 2):
         for l2 in range(n // 2):
             if l1 == l2:
                 continue
-            t1 += s[first[l1]][first[l2]]
-            t2 += s[second[l1]][second[l2]]
+            t1 += s[first[l1]][first[l2]]  # 첫 번째 팀의 능력치 합 계산
+            t2 += s[second[l1]][second[l2]]  # 두 번째 팀의 능력치 합 계산
+
+    # 두 팀 능력치 차이의 절대값 계산
     diff = abs(t1 - t2)
+
+    # 현재 차이가 최소인 경우 ans 업데이트
     if ans == -1 or ans > diff:
         ans = diff
 
+# 최소 능력치 차이 출력
 print(ans)
