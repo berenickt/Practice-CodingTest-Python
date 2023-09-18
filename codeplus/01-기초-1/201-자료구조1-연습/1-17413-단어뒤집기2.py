1c90d6950d99a2cb2547dd6921598da278cc67de
Beren <berenickt99@gmail.com>
2023-09-18 23:03:58 +0900

    FINISH:201-자료구조1-연습

--- codeplus/01-기초-1/201-자료구조1-연습/1-17413-단어뒤집기2.py
+++ codeplus/01-기초-1/201-자료구조1-연습/1-17413-단어뒤집기2.py
@@ -1,27 +1,49 @@
-# 💡 단어 뒤집기2 📚 https://www.acmicpc.net/problem/17413
+# 💡 단어 뒤집기2 📚 https://www.acmicpc.net/problem/17413  --- 스택
+"""
+태그는 뒤집지 않고, 단어만 뒤집는 프로그램
+
+📌 isalnum() : 문자열이 알파벳([a-zA-Z])과 숫자([0-9])로만 구성되었는지 확인하는 파이썬 문자열 메소드
+📌 len()     : 문자열의 길이 반환하는 함수, 
+(cf. 한글도 1로, 공백도 1로 처리함)
+(cf. 리스트나 튜플 등에서는 그 안에 속한 값의 개수를 반환)
+
+e.g.)
+len( "한글과 공백" )                  # 6
+len( [ 11, 22, 33, 44, 55, 66 ] )  # 6
+"""
 import sys
 
-# 입력 받은 문자열을 리스트로 변환하여 처리합니다.
+# 입력받은 문자열을 리스트로 변환하여 처리
 word = list(sys.stdin.readline().rstrip())
 
-# 인덱스와 시작 위치 변수를 초기화합니다.
+# 인덱스와 시작 위치 변수를 초기화
 i = 0
 start = 0
 
-# 문자열의 끝까지 반복하며 처리합니다.
+# 문자열의 끝까지 반복하며 처리
 while i < len(word):
+    # 만약 현재 문자가 "<"이라면,
     if word[i] == "<":
         i += 1
+        # "<" 다음 문자부터 ">"를 찾을 때까지 인덱스를 증가
         while word[i] != ">":
             i += 1
+        # ">"를 찾으면 인덱스를 한 번 더 증가시켜서 ">"를 건너뜀
         i += 1
+
+    # 현재 문자가 알파벳이나 숫자라면,
     elif word[i].isalnum():
         start = i
+        # 현재 문자부터 연속된 알파벳이나 숫자를 찾을 때까지 인덱스를 증가
         while i < len(word) and word[i].isalnum():
             i += 1
-        tmp = word[start:i]
-        tmp.reverse()
-        word[start:i] = tmp
+        # 단어를 임시 변수에 저장하고 뒤집습니다.
+        temp = word[start:i]
+        temp.reverse()
+        # 원래 위치에 뒤집은 단어를 다시 저장합니다.
+        word[start:i] = temp
+
+    # 현재 문자가 특수 문자나 공백이라면,
     else:
         i += 1
 
--- codeplus/01-기초-1/201-자료구조1-연습/4-17299-오등큰수📌.py
+++ codeplus/01-기초-1/201-자료구조1-연습/4-17299-오등큰수📌.py
@@ -1,30 +1,37 @@
 # 💡 오등큰수 📚 https://www.acmicpc.net/problem/17299
-# 입력된 숫자들의 빈도수를 계산하고, 오른쪽에 처음으로 나오는 숫자를 찾아 결과 리스트에 저장하는 코드
+"""
+오등큰수는 등장 횟수가 가장 많은 수 중 가장 왼쪽의 수 입니다. 없는 경우 -1을 출력
+
+
+"""
 
 freq = [0] * 1000001  # 숫자 빈도수를 저장하는 리스트를 초기화
 n = int(input())  # 숫자 개수 n을 입력받습니다.
 a = list(map(int, input().split()))  # 숫자들을 리스트로 입력받습니다.
+ans = [0] * n  # 결과를 저장할 리스트를 초기화
+stack = [0]  # 스택을 초기화하고, 첫 번째 원소의 인덱스를 스택에 넣습니다
 
 # 입력된 숫자들의 빈도수를 계산하여 freq 리스트에 저장합니다
 for i in range(n):
     freq[a[i]] += 1
 
-ans = [0] * n  # 결과를 저장할 리스트를 초기화
-s = [0]  # 스택을 초기화하고, 첫 번째 원소의 인덱스를 스택에 넣습니다
-
-# 두 번째 원소부터 끝까지 처리
+# 1부터 n까지 순회
 for i in range(1, n):
-    if not s:
-        s.append(i)
+    # 스택이 비어있으면 현재 인덱스를 스택에 추가
+    if not stack:
+        stack.append(i)
 
     # 현재 원소의 빈도수가 스택의 가장 위에 있는 원소의 빈도수보다 큰 경우
-    while s and freq[a[s[-1]]] < freq[a[i]]:
-        ans[s.pop()] = a[i]  # 스택의 가장 위 원소와 매칭되는 값을 결과 리스트에 저장
-    s.append(i)  # 현재 원소의 인덱스를 스택에 추가
+    while stack and freq[a[stack[-1]]] < freq[a[i]]:
+        # 스택의 가장 위 원소와 매칭되는 값을 결과 리스트에 저장
+        ans[stack.pop()] = a[i]
+
+    # 현재 원소의 인덱스를 스택에 추가
+    stack.append(i)
 
 # 스택에 남은 인덱스들은 빈도수에 따라 오른쪽에 더 큰 원소가 없는 경우이므로 처리
-while s:
-    ans[s.pop()] = -1
+while stack:
+    ans[stack.pop()] = -1
 
 # 결과 리스트의 값을 문자열로 변환하여 출력
 print(" ".join(map(str, ans)))
