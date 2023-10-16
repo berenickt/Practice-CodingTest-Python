44c0eb44cc42c2fdbb80c67283bbd6be9151b71d
Beren <berenickt99@gmail.com>
2023-10-16 14:30:17 +0900

    ADD:04-24480-깊이우선탐색2

---
+++ boj-dfs/type01-연결된_요소_찾기/04-24480-깊이우선탐색2.py
@@ -0,0 +1,48 @@
+### 실버2 📚 https://www.acmicpc.net/problem/24480
+"""
+정점 R에서 시작하여 깊이 우선 탐색으로 노드를 방문할 경우
+노드의 방문 순서를 출력
+
+문제 키워드 찾기: 정점, 간선, 무방향 그래프, 노드의 방문순서, 내림차순
+N : 정점의 수
+M : 간선의 수
+R : 시작정점
+"""
+### 성능이 더 빠른 input을 쓰기 위한 3줄
+import sys
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+
+def dfs(idx):
+    global visited, graph, answer, order
+    visited[idx] = True
+    answer[idx] = order
+    order += 1
+
+
+# ✅ 0. 입력 및 초기화
+N, M, R = map(int, input().split())
+MAX = 100000 + 10
+graph = [[] for _ in range(N + 1)]
+visited = [False] * MAX
+answer = [0] * MAX
+order = 1
+
+# ✅ 1. graph에 연결 정보 채우기
+for _ in range(M):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+# ✅ 2. 내림차순 정렬
+for i in range(1, N + 1):
+    graph[i] = sorted(graph[i], reverse=True)
+
+# ✅ 3. dfs(재귀함수) 호출
+dfs(R)
+
+# ✅ 4. 정답 출력
+for i in range(1, N + 1):
+    print(answer[i])
