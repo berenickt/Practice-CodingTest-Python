57944c9d126cea603c6cba3709433c320fac0488
Beren <berenickt99@gmail.com>
2023-11-01 10:58:34 +0900

    ADD:02-lv4-징검다리-43236

--- prog-high-score-kit/09-binary-search-반씩쪼개서탐색/01-lv3-입국심사-43238.py
+++ prog-high-score-kit/09-binary-search-반씩쪼개서탐색/01-lv3-입국심사-43238.py
@@ -34,25 +34,25 @@ times : [7, 10]
 """
 
 
-def check(t, times):
-    ret = 0
-    for time in times:
-        ret += t // time
-    return ret
-
-
 def solution(n, times):
     answer = 0
-    left = 0
-    right = max(times) * n  ##가장 오래걸리는 사람이 모두 처리하는 상황
+    left, right = 1, max(times) * n
+
     while left <= right:
         mid = (left + right) // 2
-        chk = check(mid, times)
-        if chk < n:
-            left = mid + 1
-        else:  ##n명보다 많이 처리할 수 있다면(answer는 될 수 있지만, 더작은 것 찾기)
+        people = 0
+        for time in times:
+            people += mid // time
+            if people >= n:
+                break
+
+        if people >= n:
             answer = mid
             right = mid - 1
+
+        elif people < n:
+            left = mid + 1
+
     return answer
 
 
--- prog-high-score-kit/09-binary-search-반씩쪼개서탐색/02-lv4-징검다리-43236.py
+++ prog-high-score-kit/09-binary-search-반씩쪼개서탐색/02-lv4-징검다리-43236.py
@@ -1,38 +1,53 @@
 # 📚 https://school.programmers.co.kr/learn/courses/30/lessons/43236
 """
+출발지점부터 도착지점까지의 거리 distance, 
+바위들이 있는 위치를 담은 배열 rocks, 
+제거할 바위의 수 n이 매개변수로 주어질 때,
+바위를 n개 제거한 뒤 각 지점 사이의 거리의 최솟값 중에 
+가장 큰 값을 return
 
+입력 #1
+distance : 25
+rocks    : [2, 14, 11, 21, 17]
+n        : 2
+
+출력 #1
+4
 """
 
 
-def check(term, rocks):
-    count = 0
-    start = 0
-    minval = 1e9
-    for i in range(len(rocks)):
-        if rocks[i] - start < term:  # start위치부터 못건널때까지 체크(지나가는 돌들 체크)
-            count += 1
-        else:
-            minval = min(minval, rocks[i] - start)
-            start = rocks[i]
-    return count, minval
-
-
 def solution(distance, rocks, n):
-    answer = 0
-    rocks.append(distance)
-    rocks.sort()
+    maxOfmin = 0
+    start, end = 0, distance
+    rocks.append(distance)  # 마지막 도착지까지 거리를 계산하기 위해 추가
+    rocks.sort()  # 오름차순 정렬
 
-    left = 0
-    right = distance
-    while left <= right:
-        mid = (left + right) // 2
-        count, minval = check(mid, rocks)
-        if count > n:
-            right = mid - 1
+    # 이분 탐색 수행
+    while start <= end:
+        mid = (start + end) // 2  # 특정한 최소거리
+        current, remove = 0, 0  # 현재 위치, 제거할 바위 수
+        min_distance = float("inf")  # mid에서 최소 거리
+
+        # 거리 구하기
+        for rock in rocks:
+            dis = rock - current
+            # mid 보다 작으면, 바위 제거
+            if dis < mid:
+                remove += 1
+            # mid 보다 작지 않다면, 현재 위치 옮기고 mid에서 최소 거리 계산
+            else:
+                current = rock
+                min_distance = min(min_distance, dis)
+
+        # n보다 많다면 mid를 줄임
+        if remove > n:
+            end = mid - 1
+        # n보다 많지 않다면 최소 거리를 answer에 저장하고 mid를 늘림
         else:
-            answer = minval
-            left = mid + 1
-    return answer
+            maxOfmin = min_distance
+            start = mid + 1
+
+    return maxOfmin
 
 
-print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
+print(solution(25, [2, 14, 11, 21, 17], 2))
