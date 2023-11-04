55673c5224fbf406665d5c302256057a21d4b9ee
Beren <berenickt99@gmail.com>
2023-11-04 16:27:55 +0900

    ADD:08-implementation(구현)

---
+++ this-is-coding-test/08-implementation(구현)/02-상하좌우-(시뮬레이션).py
@@ -0,0 +1,29 @@
+"""📍 simulation
+알고리즘을 각 한 단계씩 직접 수행하는 알고리즘. 
+Brute_force와 함께 구현의 핵심 알고리즘이 되는 경우가 많다.
+N x N Matrix 문제로 자주 출제 됨.
+"""
+# N 입력받기
+n = int(input())
+x, y = 1, 1
+plans = input().split()
+
+# L, R, U, D에 따른 이동 방향
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ["L", "R", "U", "D"]
+
+# 이동 계획을 하나씩 확인
+for plan in plans:
+    # 이동 후 좌표 구하기
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    # 공간을 벗어나는 경우 무시
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    # 이동 수행
+    x, y = nx, ny
+
+print(x, y)
