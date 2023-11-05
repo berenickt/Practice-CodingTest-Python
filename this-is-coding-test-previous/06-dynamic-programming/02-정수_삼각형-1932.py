cada733cc5557d2a15b97d56404c41a3bbb12692
Beren <berenickt99@gmail.com>
2023-11-06 00:32:59 +0900

    ADD:06-dynamic-programming

---
+++ this-is-coding-test-previous/06-dynamic-programming/02-정수_삼각형-1932.py
@@ -0,0 +1,24 @@
+# 📚 https://www.acmicpc.net/problem/1932
+n = int(input())
+dp = []  # 다이나믹 프로그래밍을 위한 DP 테이블 초기화
+
+for _ in range(n):
+    dp.append(list(map(int, input().split())))
+
+# 다이나믹 프로그래밍으로 2번째 줄부터 내려가면서 확인
+for i in range(1, n):
+    for j in range(i + 1):
+        # 왼쪽 위에서 내려오는 경우
+        if j == 0:
+            up_left = 0
+        else:
+            up_left = dp[i - 1][j - 1]
+        # 바로 위에서 내려오는 경우
+        if j == i:
+            up = 0
+        else:
+            up = dp[i - 1][j]
+        # 최대 합을 저장
+        dp[i][j] = dp[i][j] + max(up_left, up)
+
+print(max(dp[n - 1]))
---
+++ this-is-coding-test-previous/06-dynamic-programming/04-병사_배치하기-18353.py
@@ -0,0 +1,17 @@
+# 📚 https://www.acmicpc.net/problem/18353
+n = int(input())
+array = list(map(int, input().split()))
+# 순서를 뒤집어 '최장 증가 부분 수열' 문제로 변환
+array.reverse()
+
+# 다이나믹 프로그래밍을 위한 1차원 DP 테이블 초기화
+dp = [1] * n
+
+# 가장 긴 증가하는 부분 수열(LIS) 알고리즘 수행
+for i in range(1, n):
+    for j in range(0, i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+# 열외해야 하는 병사의 최소 수를 출력
+print(n - max(dp))
