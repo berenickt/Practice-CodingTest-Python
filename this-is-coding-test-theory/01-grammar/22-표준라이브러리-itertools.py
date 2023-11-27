0e2d60f9cccde702bbbef38e574b15c02ccc3440
Beren <berenickt99@gmail.com>
2023-11-27 13:03:19 +0900

    ADD:readme

--- this-is-coding-test-theory/01-grammar/22-표준라이브러리-itertools.py
+++ this-is-coding-test-theory/01-grammar/22-표준라이브러리-itertools.py
@@ -1,9 +1,12 @@
-"""
-permutation() : 순열
+"""Itertools, 이터툴즈
+파이썬에서 반복되는 데이터(iterable 한 데이터)를 처리하는 기능을 포함
+즉 이터러블(iterable)한 데이터는 반복문에서 사용할 수 있는 데이터를 의미
+
+💡 permutation() : 순열
 - list iterable 객체에서 r개의 데이터를 뽑아 
 - 일렬로 나열하는 모든 경우(순열)
 
-combination() : 조합
+💡 combination() : 조합
 - list iterable 객체에서 r개의 데이터를 뽑아 
 - 순서를 고려하지 않고 나열하는 모든 경우(조합)
 """
